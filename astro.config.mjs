25fe86c93100fa14ce980558fcbce452005140e7
bingKegeta <sohamg24@gmail.com>
2025-03-21 18:01:54 -0400

    Resolve CORS issue for VPS deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
 import preact from "@astrojs/preact";
-
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: ["boofoo.store", "www.boofoo.store", "localhost"],
+    },
+  },
+  site: "https://boofoo.store",
+  output: "static", // Explicitly setting to static mode
   integrations: [tailwind(), svelte(), preact()],
-  adapter: vercel(),
+  adapter: node({ mode: "standalone" }),
 });
