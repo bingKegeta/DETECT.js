eebfb275f0d9d7cbcad43908628317c66e59bef8
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2025-03-24 01:27:32 -0400

    Revert changes

--- src/components/VideoUploadProcessing.svelte
+++ src/components/VideoUploadProcessing.svelte
@@ -1,4 +1,5 @@
 
+
 <script lang="ts">
   import { drawLandmarks } from "@mediapipe/drawing_utils";
   import { FaceMesh, type Results } from "@mediapipe/face_mesh";
@@ -69,8 +70,6 @@
    export const shouldShowGraph = writable(false);
 
    let affineTransformEnabled = writable(false);
-
-   let timestamp = 0;
 
    // Log the settings whenever they change
    userSettings.subscribe((settings: any) => {
@@ -217,14 +216,11 @@
             });
           }
 
-          timestamp = performance.now();
-
           // Smoothing iris positions
-          const { normX, normY } = getNormalizedIrisPosition(
+          const { normX, normY, timestamp } = getNormalizedIrisPosition(
             landmarks,
             processingCanvas.width,
             processingCanvas.height,
-            timestamp
           );
 
           // Apply smoothing to the iris position
