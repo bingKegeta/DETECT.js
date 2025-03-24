733fee254cc21ba333b2e4ba2aae1003690cc293
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2025-03-24 01:26:49 -0400

    Revert changes

--- src/components/Webcam.svelte
+++ src/components/Webcam.svelte
@@ -66,8 +66,6 @@
 
   let affineTransformEnabled = writable(false);
 
-  let timestamp = 0;
-
   // Log the settings whenever they change
   userSettings.subscribe((settings: any) => {
     console.log("User settings:", settings);
@@ -253,14 +251,11 @@
             lineWidth: 1,
           });
 
-          timestamp = performance.now();
-
           // Smoothing iris positions
-          const { normX, normY } = getNormalizedIrisPosition(
+          const { normX, normY, timestamp } = getNormalizedIrisPosition(
             landmarks,
             canvasEl.width,
             canvasEl.height,
-            timestamp
           );
 
           // Apply smoothing to the iris position
