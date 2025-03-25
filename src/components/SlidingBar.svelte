91e7692b09383e60505c5fbeeb71588813833c07
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2025-03-25 19:23:04 -0400

    Update SlidingBar.svelte to have wider range of sensitivity

--- src/components/SlidingBar.svelte
+++ src/components/SlidingBar.svelte
@@ -37,15 +37,15 @@
   <input
     type="range"
     class="w-full appearance-none h-2 bg-gray-300 rounded-lg slider-thumb"
-    min="0.75"
-    max="1.25"
+    min="0.50"
+    max="2.00"
     step="0.01"
     bind:value={sensitivity}
     id="sensitivity-slider"
   />
   <div class="flex justify-between text-xs text-gray-400 mt-1">
-    <span>0.75</span>
+    <span>0.50</span>
     <span class="text-white font-bold">{sensitivity.toFixed(2)}</span>
-    <span>1.25</span>
+    <span>2.00</span>
   </div>
 </div>
